06ddbcf87da3628713f7d14fadc37b703fdc4d4d
Hin Lam <hlam@pivotal.io>
2018-10-03 23:24:01 -0700

    Added env dump, throw exceptions, self-kill

---
+++ DotNetCoreWebAppSample/DumpAllEnvController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreWebAppSample
+{
+    public class DumpAllEnvController : Controller
+    {
+        public IDictionary Index()
+        {
+            String output = "";
+            IDictionary d = System.Environment.GetEnvironmentVariables();
+            foreach(DictionaryEntry entry in d)
+            {
+                output += entry.Key + ": " + entry.Value;
+                Console.Out.WriteLine(output);
+                output += "<br/>";
+            }
+            return d;
+        }
+    }
+}
---
+++ DotNetCoreWebAppSample/ErrorController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetCoreWebAppSample
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/
+        public IActionResult Kill()
+        {
+            Environment.Exit(-1);
+            return null;
+        }
+
+        public IActionResult ThrowException()
+        {
+            throw new SystemException("This is a generated system exception in ErrorController.ThrowException");
+        }
+    }
+}
